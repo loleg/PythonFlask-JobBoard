94b7e64b1c3601b2184b706f697cda77a3ae3c22
Robert McDaniel <mcdaniel.robert@gmail.com>
2019-06-12 01:33:57 -0500

    Completed the module

--- jobs/app.py
+++ jobs/app.py
@@ -14,7 +14,7 @@ def open_connection():
     return connection
 
 def execute_sql(sql, values=(), commit=False, single=False):
-    connection = open_conection()
+    connection = open_connection()
     cursor = connection.execute(sql, values)
     if commit == True:
         results = connection.commit()
