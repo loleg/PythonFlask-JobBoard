6d97266735bf8c7b6bf582a61d8fb2f3524995ef
Oleg Lavrovsky <oleg@utou.ch>
2019-07-20 14:42:56 +0200

    New job post

--- jobs/app.py
+++ jobs/app.py
@@ -47,7 +47,7 @@ def employer(employer_id):
     employer = execute_sql('SELECT * FROM employer WHERE id=?', [employer_id], single=True)
     jobs = execute_sql('SELECT job.id, job.title, job.description, job.salary FROM job JOIN employer ON employer.id = job.employer_id WHERE employer.id = ?', [employer_id])
     reviews = execute_sql('SELECT review, rating, title, date, status FROM review JOIN employer ON employer.id = review.employer_id WHERE employer.id = ?', [employer_id])
-    return render_template('employer.html', employer=employer, jobs=jobs, reviews=reviews)   
+    return render_template('employer.html', employer=employer, jobs=jobs, reviews=reviews)
 
 @app.route('/employer/<employer_id>/review', methods=('GET', 'POST'))
 def review(employer_id):
@@ -63,3 +63,18 @@ def review(employer_id):
 
     return render_template('review.html', employer_id=employer_id)
 
+@app.route('/employer/<employer_id>/post', methods=('GET', 'POST'))
+def newjob(employer_id):
+    if request.method == 'POST':
+        title = request.form['title']
+        description = request.form['description']
+        salary = request.form['salary']
+
+        date = datetime.datetime.now().strftime("%m/%d/%Y")
+        execute_sql(
+            'INSERT INTO job (title, description, salary, date, employer_id) VALUES ' +
+            '(?, ?, ?, ?, ?)',
+            (title, description, salary, date, employer_id), commit=True)
+        return redirect(url_for('employer', employer_id=employer_id))
+
+    return render_template('newjob.html', employer_id=employer_id)
